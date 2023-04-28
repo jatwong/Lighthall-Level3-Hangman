67545332b6af451fd16ea373ecb622f6f275bb6b
Justina Wong <justinawong97@gmail.com>
2023-04-27 18:29:44 -0700

    landing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/UI/Header";
 import RankGameLogin from "./components/pages/RankGameLogin";
-import classes from './App.module.css'
+import classes from "./App.module.css";
 import Game from "./components/pages/game";
 import Challenge from "./components/pages/Challenge";
 import Landing from "./components/pages/Landing";
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -2,14 +2,18 @@ import Leaderboard from "../Leaderboard";
 import classes from "./Landing.module.css";
 
 const Landing = () => {
+  
+  const playGameHandler = () => {};
+  const createGameHandler = () => {};
+
   return (
     <div className={classes.page}>
       <Leaderboard />
       <div>
         <h1 className={classes.logo}>HANGMAN</h1>
         <div className={classes.options}>
-          <button>Play Ranked</button>
-          <button>Challenge a friend</button>
+          <button onClick={playGameHandler}>Play Ranked</button>
+          <button onClick={createGameHandler}>Challenge a friend</button>
         </div>
       </div>
     </div>
